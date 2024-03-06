aeba195a95f38020bd62bdc600bd24b8aa524897
Mohamed Yassine Rejeb <mohamedyassine.rejeb@gmail.com>
2024-03-06 14:41:19 +0100

    march_6th_2024

---
+++ fssp_django/api/serializers.py
@@ -0,0 +1,49 @@
+### Sterializers for the models ###
+from rest_framework import serializers
+
+
+class userAccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = userAccount
+        fields = '__all__'
+
+class emailVerificationKeySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = emailVerificationKey
+        fields = '__all__'
+    
+class objectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = object
+        fields = '__all__'
+
+class secretSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = secret
+        fields = '__all__'
+    
+class fileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = file
+        fields = '__all__'
+    
+class notificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = notification
+        fields = '__all__'
+
+class activityLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = activityLog
+        fields = '__all__'
+
+# class sessionSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = session
+#         fields = '__all__'
+    
+class shareSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = share
+        fields = '__all__'
+
---
+++ fssp_vuejs/src/components/Logout.vue
@@ -0,0 +1,35 @@
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+import store from "../store";
+
+const DJANGO_API_URL = "http://127.0.0.1:8000/api/";
+
+function sendLogoutRequest() {
+  axios
+    .get(DJANGO_API_URL + "logout", {withCredentials: true,})
+    .then((response) => {
+      console.log(response);
+      // store.state.authenticated = false;
+      // authenticated = store.state.authenticated;
+      window.location.href = "/login";
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+const isLoading = ref(false);
+
+
+isLoading.value = true;
+setTimeout(() => {
+isLoading.value = false;
+sendLogoutRequest();
+}, 1000);
+
+</script>
+
+<template>
+
+</template>
